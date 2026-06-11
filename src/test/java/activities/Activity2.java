78be391f63e122fb707afc1a39f2fd7debdc3f4b
LakshmiN <LakshmiN@IBM-FH1XR24.j9d-in.ibm.com>
2026-06-11 16:28:10 +0530

    Activity2: Completed Goal: Print the url of the header image to the console

--- src/test/java/activities/Activity2.java
+++ src/test/java/activities/Activity2.java
@@ -1,10 +1,40 @@
 package activities;
 
+import org.openqa.selenium.By;
+
+//Goal: Print the url of the header image to the console
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
 public class Activity2 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	// Declare WebDriver
+	WebDriver driver;
+	
+	@BeforeClass
+	public void setup() {
+		// Initialize the driver object
+		driver = new FirefoxDriver();
+		
+		// Open the page
+		driver.get("http://hrm.local:3050/symfony/web/index.php/auth/login");
+	}
+	
+	@Test
+	public void imageURL() {
+		WebElement logo = driver.findElement(By.id("divLogo"));
+		Assert.assertNotNull(logo, "Logo is NOT displayed");
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		// Close the browser
+		driver.quit();
 	}
 
 }
