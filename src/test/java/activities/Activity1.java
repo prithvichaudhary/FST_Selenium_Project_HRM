51715ac620323c28cf931813785fd6fb361715d4
YamunaReddy358 <hemuyamuna58@gmail.com>
2026-06-12 14:00:26 +0530

    Refactor Activity1 for WebDriver setup and title check
    Refactor Activity1 class to set up WebDriver and verify website title.

--- src/test/java/activities/Activity1.java
+++ src/test/java/activities/Activity1.java
@@ -1,45 +1,34 @@
 package activities;
 
-import org.testng.annotations.Test;
-import org.testng.annotations.BeforeClass;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.testng.Assert;
 import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class Activity1 {
+	WebDriver driver;
 
-	public class FirstTest {
-		//Declare WebDriver
-		WebDriver driver;
-		
-		@BeforeClass
-		public void setUp() {
-			//Initialize the driver object
-			driver = new FirefoxDriver();
-			
-			//Open page
-			driver.get("https://training-support.net/");
-		}
-		
-	  @Test(priority = 1)
-	  public void verifyPageTitle() {
-		  //Asset the page title
-		  Assert.assertEquals(driver.getTitle(), "Training Support");
-	  }
-	  
-	  @Test(priority = 2)
-	  public void clickAboutBtn() {
-		  //Asset the page title
-		  driver.findElement(By.linkText("About Us")).click();
-		  Assert.assertEquals(driver.getTitle(), "About Training Support");
-	  }
+	//Setup browser
+	@BeforeClass
+	public void setUp() {
+		driver = new FirefoxDriver();
 
-	  @AfterClass
-	  public void tearDown() {
-		  driver.quit();
-	  }
+		//Open URL
+		driver.get("http://hrm.local:3050");
+	}
 
+	//Verify title
+	@Test
+	public void verifyWebsiteTitle() {
+		String actualTitle = driver.getTitle();
+		String expectedTitle = "OrangeHRM";
+
+	}
+
+	//Close browser
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
 	}
 }
